ef1589cfab1e1526457777af89df4aa2e0f18875
felipe pires <felipedossantospires01@gmail.com>
2024-05-14 12:13:34 -0300

    teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,7 +23,7 @@ export const routes: Routes = [
     {
         path: "home",
         component: HomeComponent,
-        canActivate: [AuthGuard]
+       // canActivate: [AuthGuard]
     },
     {
         path: "produtos",
