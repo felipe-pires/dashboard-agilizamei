c8eabe0751dda3f7f538625d3efb4a5592c84e54
felipe pires <felipedossantospires01@gmail.com>
2024-05-19 22:28:09 -0300

    fix: menu

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -4,25 +4,39 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginService } from '../../../services/login/login.service';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [MatToolbarModule, MatIconModule, MatButtonModule, CommonModule],
+  providers: [NavComponent,],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-   mostrarMenu = false;
+  mostrarMenu = false;
 
-  constructor(private service: LoginService) {
+  constructor(
+    private service: LoginService,
+    private navComponent: NavComponent
+  ) {
     this.service.mostrarMenu.subscribe((resposta) => {
       this.mostrarMenu = resposta;
     });
-
-  }
-  teste(){
-    return this.service.isLogin()
   }
 
+  toggleMenu(): void {
+    if (this.navComponent) {
+      alert('teste')
+      this.navComponent.toggleDrawer();
+      alert('teste')
+    } else {
+      console.error('NavComponent não está definido.');
+    }
+  }
+
+  teste() {
+    return this.service.isLogin();
+  }
 }
--- src/app/components/template/nav/nav.component.ts
+++ src/app/components/template/nav/nav.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { LoginService } from '../../../services/login/login.service';
 import { CommonModule } from '@angular/common';
 import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-nav',
@@ -18,28 +24,28 @@ import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
     RouterLink,
     RouterLinkActive,
     CommonModule,
+    MatButtonModule
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
 export class NavComponent {
-  mostrarMenu = false;
+  @ViewChild('drawer') drawer!: MatDrawer;
 
-  constructor(private service: LoginService, private router: Router) {
-    this.service.mostrarMenu.subscribe((resposta) => {
-      this.mostrarMenu = resposta;
-    });
+  constructor(private service: LoginService, private router: Router) {}
+
+  toggleDrawer(): void {
+    if (this.drawer) {
+      this.drawer.toggle();
+    }
   }
-
   isLogin() {
     return this.service.isLogin();
   }
-  
+
   logout() {
     return this.service.logout();
   }
-
-  
 
   getContentClass() {
     return this.router.url.includes('/login')
